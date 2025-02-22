d0d1ace5ff9e42404440798453f7597b11fc6ec3
yashbharadwaj52 <yash7982022261@gmail.com>
2025-02-22 10:54:40 +0530

    authentication secured

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true, // Important for Clerk with Next.js 15
+  },
   images: {
     domains: ["res.cloudinary.com","img.clerk.com"],
   },
